862fdf7e153fc45cc89af4e6f23f11336a987abb
Daan de jager <daandejager2002@gmail.com>
2020-06-02 21:25:34 +0200

    1 KLaar

---
+++ 1-Arrays/taak01/index.php
@@ -0,0 +1,9 @@
+<?php 
+$voornaam = array("Daan"=>"Daan" , "Randy"=>"Randy" , "Karma"=>"Karma");
+$achternaam = array("Daan"=>"De Jager" , "Randy"=>"Ofusu" , "Karma"=>"King");
+$Leeftijd = array("Daan"=>"16" , "Randy"=>"25" , "Karma"=>"16");
+$klas = array("Daan"=>"9C" , "Randy"=>"9C" , "Karma"=>"9C");
+echo "Gegevens Daan:" . "<br>" . "Voornaam:" . $voornaam['Daan'] . "<br>"
+. "Achternaam:" . $achternaam['Daan'] . "<br>" . "Leeftijd:" . $Leeftijd['Daan']
+. "<br>" . "Klas:" . $klas['Daan'];
+?>
