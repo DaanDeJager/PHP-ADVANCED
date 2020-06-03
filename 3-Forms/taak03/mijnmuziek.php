6ec9d63304a50fcd2a7dec966c5cd328d77626f8
Daan de jager <daandejager2002@gmail.com>
2020-06-03 10:08:47 +0200

    klaar met 3

---
+++ 3-Forms/taak03/mijnmuziek.php
@@ -0,0 +1,13 @@
+
+<html>
+<body>
+
+Artiesten: <?php echo $_GET["artist"]; ?><br>
+Titel: <?php echo $_GET["Titel"]; ?><br>
+Jaar van Uitkomst: <?php echo $_GET["JvA"]; ?><br>
+Muzieksoort:  <?php echo $_GET["soort"]; ?><br>
+
+
+</body>
+</html>
+<!-- http://localhost:8888/php/Periode%204%20EAGLEDEV/PHP-ADVANCED/3-Forms/taak03/mijnmuziek.php?artist=Green+day&Titel=Basket+Case&JvA=1994&soort=Rock -->
